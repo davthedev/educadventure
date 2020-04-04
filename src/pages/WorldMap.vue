7e15a8d6702e65724e78028b4fd1bcda42416c68
David Giardi <david.giardi.eng@gmail.com>
2020-04-04 20:34:41 +0200

    Custom pins

--- src/pages/WorldMap.vue
+++ src/pages/WorldMap.vue
@@ -4,7 +4,12 @@
       <l-map ref="map" :zoom="zoom" :center="center" :options="mapOptions" :crs="crs">
         <l-tile-layer :url="url" :attribution="attribution"/>
 
-      <l-marker :lat-lng="[-50, 50]" />
+        <l-marker v-for="(pin, k) in pins" :key="k" :lat-lng="pin.latlng" :icon="icon">
+          <l-popup >
+            <p>{{pin.name}}</p>
+            <q-btn color="primary" @click="accessPinData(pin.datakey)">Go there</q-btn>
+          </l-popup>
+        </l-marker>
       </l-map>
     </div>
 
@@ -12,9 +17,9 @@
 </template>
 
 <script>
-import { LMap, LTileLayer, LMarker } from 'vue2-leaflet'
+import { LMap, LTileLayer, LMarker, LPopup, LIcon } from 'vue2-leaflet'
 import L from 'leaflet'
-import { CRS } from 'leaflet'
+import { CRS, icon } from 'leaflet'
 import 'leaflet/dist/leaflet.css'
 
 export default {
@@ -23,13 +28,24 @@ export default {
     LMap,
     LTileLayer,
     LMarker,
+    LPopup,
+    LIcon,
   },
   data () {
     return {
+
+
+      pins: [
+        {name:"Challenge 1", datakey:0, latlng:[-50, 50]},
+        {name:"Challenge 2", datakey:1, latlng:[-30, 50]},
+      ],
+
+
       crs: CRS.Simple,
       map: null,
       mapOptions: {
         scrollWheelZoom: false,
+        doubleClickZoom: false,
         maxBounds: [[0, 0], [-128, 256]],
         noWrap: true,
         zoomControl: false,
@@ -39,12 +55,26 @@ export default {
       maxZoom: 4,
       center: [-50, 100],
       url: 'statics/maps/fantasy/12/tile_{x}-{y}.png',
+
+      icon: icon({
+        iconUrl: "statics/maps/pins/Pin1.png",
+        iconSize: [52, 84],
+        iconAnchor: [26, 84],
+        popupAnchor: [0, -84],
+      }),
     }
   },
   mounted() {
     this.$nextTick(() => {
       this.map = this.$refs.map.mapObject // work as expected
     })
+  },
+  methods: {
+    accessPinData: function(datakey) {
+      console.log("Open pin data " + datakey)
+
+      this.$router.push('/mission')
+    }
   }
 }
 </script>
